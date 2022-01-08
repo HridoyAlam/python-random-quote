816cf3c1d1cf4806d5ce597bf6b63c0d88b12389
HridoyAlam <optimus5289@gmail.com>
2022-01-08 16:23:07 +0600

    file

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
 def primary():
-  print("Keep it logically awesome.")
+  #print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("C:/Users/hrido\Desktop/git/python-random-quote/quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  print(quotes[0])
+  print(quotes)
   
 
 if __name__== "__main__":
